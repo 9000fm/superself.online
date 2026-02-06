86ede881fa46e7451586f07c70da6be9866a2cdb
Ubuntu <ubuntu@ip-172-31-0-150.us-east-2.compute.internal>
2026-02-06 14:37:39 +0000

    fix: remove /fm route temporarily (missing deps)

--- src/app/fm/sections/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Preloader } from './Preloader';
-export { default as Hero } from './Hero';
-export { default as Work } from './Work';
-export { default as About } from './About';
-export { default as Experiments } from './Experiments';
-export { default as Contact } from './Contact';
